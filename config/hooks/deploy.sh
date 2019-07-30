4df9aae290769662e44c17dcba55e3f2348d9a2f
«Jka4» <plotnik1295@gmail.com>
2019-07-30 13:36:18 +0300

    fix

--- config/hooks/deploy.sh
+++ config/hooks/deploy.sh
@@ -1,7 +1,9 @@
-git add .
-git commit -m 'Solution'
-git push origin fe_mar19_vova
+#!/usr/bin/env bash
 
+cp -r backstop_data/ src/report
 git add src/
-git commit -m "solution"
+git commit -m "deploy static content"
 git subtree push --prefix src origin gh-pages
+./node_modules/.bin/rimraf src/report
+git add src/
+git commit -m "removed report after deploy"
